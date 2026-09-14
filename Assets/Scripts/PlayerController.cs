bb19bd2e60188e76083ea89aa0e9288e1e062384
Ethan <ehowe1215@gmail.com>
2026-09-14 17:51:50 -0400

    Start Prototype

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float playerSpeed;
+    private Rigidbody2D rb;
+    private Vector2 playerDirection;
+
+    void Start ()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float directionY = Input.GetAxisRaw("Vertical");
+        playerDirection = new Vector2(0, directionY).normalized;
+    }
+
+   void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(0, playerDirection.y * playerSpeed);
     }
 }
